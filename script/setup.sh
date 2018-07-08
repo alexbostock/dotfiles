bdea1a0190b189bc0f3c63a3e7f8b1ade2efb107
alexbostock <alex@alexbostock.co.uk>
2018-07-08 10:26:06 +0100

    Add setup script

---
+++ script/setup.sh
@@ -0,0 +1,10 @@
+# Assume we have already done:
+
+# cd projects
+# git clone https://github.com/alexbostock/dotfiles.git
+
+cd ~
+
+ln -s projects/dotfiles/.bash_profile
+ln -s projects/dotfiles/.config
+ln -s projects/dotfiles/.git-prompt
